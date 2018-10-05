72b870170b2be25e578338f974404e4c68f67d80
Antonio Andrade Gomes <gomesmax1997@gmail.com>
2018-10-04 23:05:46 -0300

    Diretorios que o professor de Engenharia de Software irá pedir.2

---
+++ test/TesteControle.java
@@ -0,0 +1,13 @@
+import junit.framework.TestCase;
+
+public class TesteControle extends TestCase {
+    
+    protected void setUp() throws Exception{
+        super.setUp();
+        //resto do codigo 
+    }
+    
+    protected void tearDown() throws Exception{
+        //resto do codigo
+    }
+}
